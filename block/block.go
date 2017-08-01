fcf3ea7da5638a1a356efcabb93f036c3346467c
Pavel Bazin <pvlbzn@gmail.com>
2017-08-02 01:23:06 +0200

    Refactoring

--- block/block.go
+++ block/block.go
@@ -1,14 +1,19 @@
 package block
 
 import (
+	"bufio"
+	"crypto/sha256"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"strconv"
+	"strings"
 )
 
 type Block struct {
 	Transactions []Transaction
 	PreviousHash string
-	Number       string
+	Number       int
 }
 
 type Transaction struct {
@@ -23,26 +28,154 @@ func (t *Transaction) String() string {
 	return t.From + " -> " + t.To + " : " + strconv.Itoa(t.Amount)
 }
 
+// NewTransaction is a convinience wrapper for a new transaction.
+func NewTransaction(from, to string, amount int) *Transaction {
+	return &Transaction{From: from, To: to, Amount: amount}
+}
+
+func NewBlock() (*Block, error) {
+	n, err := findLast()
+	if err != nil {
+		return nil, err
+	}
+
+	h, err := ftoh(n)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("Hash: " + h)
+
+	return &Block{Number: n + 1, PreviousHash: h}, nil
+}
+
+func findLast() (int, error) {
+	blocks, err := ioutil.ReadDir("blocks")
+	if err != nil {
+		return -1, err
+	}
+
+	var n int
+	for _, b := range blocks {
+		bn := strings.Split(b.Name(), ".")[0]
+		number, err := strconv.Atoi(bn)
+		if err != nil {
+			continue
+		}
+
+		if number > n {
+			n = number
+		}
+	}
+
+	return n, nil
+}
+
+func ftoh(n int) (string, error) {
+	f, err := ioutil.ReadFile("blocks/" + strconv.Itoa(n) + ".block")
+	if err != nil {
+		return "", err
+	}
+
+	return hash(f)
+}
+
+func hash(f []byte) (string, error) {
+	sha := sha256.New()
+	_, err := sha.Write(f)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", sha.Sum(nil)), nil
+}
+
+// String implements Stringer interface. Such that a block
+// can be printed well using fmt package.
 func (b *Block) String() string {
 	var t string
-	for i, transaction := range b.Transactions {
-		t += fmt.Sprintf("%d: %s\n", i+1, transaction.String())
+	for _, transaction := range b.Transactions {
+		t += fmt.Sprintf("%s\n", transaction.String())
 	}
 	return t + b.PreviousHash
 }
 
-func (b *Block) AddTransaction(t *Transaction) error {
-
+// AddTransaction to transaction collection
+func (b *Block) AddTransaction(t *Transaction) {
+	b.Transactions = append(b.Transactions, *t)
 }
 
+// Write a block into a file
 func (b *Block) Write() error {
+	data := []byte(b.String())
+	fname := "blocks/" + strconv.Itoa(b.Number) + ".block"
 
+	if err := ioutil.WriteFile(fname, data, 0600); err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func (b *Block) Read() (*Block, error) {
+// Read a block from a file
+func Read(n int) (*Block, error) {
+	fname := "blocks/" + strconv.Itoa(n) + ".block"
+	f, err := os.Open(fname)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
 
+	// Scan each line into data array
+	scanner := bufio.NewScanner(f)
+	var data []string
+	for scanner.Scan() {
+		data = append(data, scanner.Text())
+	}
+
+	// Read hash
+	// Hash is known to be the last line in the block
+	h := data[len(data)-1]
+
+	// Read transactions
+	var transactions []Transaction
+	for n := len(data) - 2; n > 0; n++ {
+		l := strings.Split(data[n], " ")
+		amount, err := strconv.ParseInt(l[4], 10, 32)
+		if err != nil {
+			return nil, err
+		}
+		t := NewTransaction(l[0], l[2], int(amount))
+		transactions = append(transactions, *t)
+	}
+
+	return &Block{Transactions: transactions, PreviousHash: h, Number: n}, nil
 }
 
-func ValidateChain(lastBlock int) bool {
+// ValidateChain checks chain hash validity
+func ValidateChain() (bool, error) {
+	n, err := findLast()
+	if err != nil {
+		return false, err
+	}
 
+	for n > 0 {
+		h, err := ftoh(n - 1)
+		if err != nil {
+			return false, err
+		}
+
+		b, err := Read(n)
+		if err != nil {
+			return false, err
+		}
+
+		if b.PreviousHash != h {
+			return false, nil
+		}
+
+		n--
+	}
+
+	return true, nil
 }
--- main.go
+++ main.go
@@ -1,149 +1,22 @@
 package main
 
 import (
-	"bufio"
-	"crypto/sha256"
 	"fmt"
-	"io/ioutil"
-	"os"
-	"strconv"
-	"strings"
+
+	"github.com/pvlbzn/blockchan/block"
 )
 
-var nblock int
-
-type Block struct {
-	From         string
-	To           string
-	Amount       int
-	PreviousHash string
-}
-
-func (b *Block) toString() string {
-
-	return b.From + " -> " + b.To + " : " + strconv.Itoa(b.Amount) + "\n" + b.PreviousHash
-}
-
-func (b *Block) save() error {
-	data := []byte(b.toString())
-	fname := "blocks/" + strconv.Itoa(nblock) + ".block"
-
-	fmt.Println(fname)
-
-	if err := ioutil.WriteFile(fname, data, 0600); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// Hash block # n
-func hash(n int) (string, error) {
-	fname := "blocks/" + strconv.Itoa(n) + ".block"
-	f, err := ioutil.ReadFile(fname)
-	if err != nil {
-		return "", err
-	}
-
-	sha := sha256.New()
-	_, err = sha.Write(f)
-	if err != nil {
-		return "", err
-	}
-
-	fmt.Printf("%x", sha.Sum(nil))
-
-	h := fmt.Sprintf("%x", sha.Sum(nil))
-
-	return h, nil
-}
-
-// Read n numbered block
-func read(n int) (*Block, error) {
-	fname := "blocks/" + strconv.Itoa(n) + ".block"
-	f, err := os.Open(fname)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	scanner := bufio.NewScanner(f)
-
-	// Scan the first line: transaction
-	scanner.Scan()
-	firstLine := strings.Split(scanner.Text(), " ")
-
-	amount, err := strconv.ParseInt(firstLine[4], 10, 32)
-	if err != nil {
-		return nil, err
-	}
-
-	// Scan the next line: previous hash
-	scanner.Scan()
-	nextLine := scanner.Text()
-
-	fmt.Println("next line\t" + nextLine)
-
-	b := Block{From: firstLine[0], To: firstLine[2], Amount: int(amount), PreviousHash: nextLine}
-
-	return &b, nil
-}
-
-// Calculate last transaction hash
-// Fill in the transaction
-// Add the last hash
-// Save
-func MakeTransaction(from, to string, amount int) error {
-	lh, err := hash(nblock - 1)
-	if err != nil {
-		return err
-	}
-
-	b := &Block{From: from, To: to, Amount: amount, PreviousHash: lh}
-	if err := b.save(); err != nil {
-		return err
-	}
-
-	nblock++
-
-	return nil
-}
-
-func ValidateChain(nblock int) bool {
-	// calculate hash of nblock-1, compare it to hash from n block
-	// do until genezis (0)
-	for nblock > 0 {
-		h, err := hash(nblock - 1)
-		if err != nil {
-			panic(err)
-		}
-
-		b, err := read(nblock)
-		if err != nil {
-			panic(err)
-		}
-
-		if b.PreviousHash != h {
-			return false
-		}
-
-		nblock--
-	}
-	return true
-}
-
 func main() {
-	nblock = 1
+	b, _ := block.NewBlock()
+	b.AddTransaction(block.NewTransaction("Sam", "Moe", 51))
+	b.Write()
 
-	if err := MakeTransaction("Mike", "Erika", 15); err != nil {
-		panic(err)
-	}
-	if err := MakeTransaction("Steve", "John", 2); err != nil {
-		panic(err)
-	}
-	if err := MakeTransaction("Morr", "Jom", 9); err != nil {
-		panic(err)
+	for n := 0; n < 5; n++ {
+		b, _ := block.NewBlock()
+		b.AddTransaction(block.NewTransaction("Sam", "Moe", n*3))
+		b.Write()
 	}
 
-	fmt.Print(ValidateChain(nblock - 1))
+	s, _ := block.ValidateChain()
+	fmt.Println(s)
 }
